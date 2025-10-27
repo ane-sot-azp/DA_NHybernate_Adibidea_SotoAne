578dece5533c1dd25f182d620cadba951b9270c7
Ane <ane_sot_azp@goierrieskola.org>
2025-10-27 16:43:53 +0100

    froga branch-eko eguneraketa

--- ConsolaNHibernate/Modeloak/Erabiltzailea.cs
+++ ConsolaNHibernate/Modeloak/Erabiltzailea.cs
@@ -24,6 +24,8 @@ namespace ConsolaNHibernate.Modeloak
 
         public virtual IList<Eskaria> Eskariak { get; set; } = new List<Eskaria>(); // ONE TO MANY harremana
 
+        public virtual IList<Rola> Rolak{ get; set; } = new List<Rola>(); // MANY TO MANY harremana
+
 
 
     }
